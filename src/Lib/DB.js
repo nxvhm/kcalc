9a92d3826f0ea9358a49e5e8111e5a43ca2d5f52
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2021-11-08 23:32:44 +0200

    SQLite DB

--- src/Lib/DB.js
+++ src/Lib/DB.js
@@ -1,3 +1,33 @@
-import { openDatabase } from 'react-native-sqlite-storage';
+import SQLite  from 'react-native-sqlite-storage';
 
-let db = openDatabase({ name: 'kcalc.db' });
+const db = SQLite.openDatabase({ name: 'kcalc.db' });
+
+class Database {
+    db;
+
+    constructor(db) {
+        this.db = db;
+    }
+
+    exec(sql, params) {
+        return new Promise( (resolve, reject) => {
+            this.db.transaction(tx => {
+                tx.executeSql(sql , params, (db, results) => {
+                    resolve(results);
+                }, error => {
+                    reject(error);
+                });
+            })
+        })
+    }
+
+
+
+    test() {
+        console.log('dasdasdas');
+    }
+}
+
+export default new Database(db);
+
+
