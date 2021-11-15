110005cd04db0667e708d74b3ca3172f877a46d0
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2021-11-15 22:57:23 +0200

    Calc. selected product kcal, add product to meal, display current meal data @ UI

--- src/Screens/AddMeal.js
+++ src/Screens/AddMeal.js
@@ -12,7 +12,15 @@ class AddMeal extends React.Component {
     super(props);
     this.state = {
       productsResult: [],
-      selectedProduct: null
+      selectedProduct: null,
+      meal: {
+        fats: 0,
+        carbs: 0,
+        sugars: 0,
+        proteins: 0,
+        calories: 0,
+        products: [],
+      }
     }
   }
 
@@ -81,6 +89,7 @@ class AddMeal extends React.Component {
 
     // Add amount field to selected product
     selectedProduct.amount = null;
+    selectedProduct.per_amount = null;
 
     // Empty selected products
     productsResult = [];
@@ -92,7 +101,82 @@ class AddMeal extends React.Component {
   setSelectedProductAmount(amount) {
     let {selectedProduct} = this.state;
     selectedProduct.amount = amount;
+    selectedProduct.per_amount = (selectedProduct.amount/100)*selectedProduct.calories;
+    selectedProduct.per_amount = Math.round(selectedProduct.per_amount*100)/100;
     this.setState({selectedProduct})
+  }
+
+  caloriesPerAmountParagraph(selectedProduct) {
+
+    if (selectedProduct.amount) {
+      return <Paragraph>
+        Calories per amount: {selectedProduct.amount}gr./{selectedProduct.per_amount}kcal
+      </Paragraph>
+    }
+
+    return null;
+  }
+
+  addToMeal() {
+    let {meal, selectedProduct} = this.state;
+
+    delete selectedProduct.photo;
+    delete selectedProduct.description;
+
+    // Save product data to meal
+    meal.products.push(selectedProduct);
+
+    // Calculate meal nutritions
+    meal.calories += selectedProduct.per_amount;
+
+    meal.carbs += Math.round(
+      ((selectedProduct.amount/100)*selectedProduct.carbs)*100
+    )/100;
+
+    meal.sugars += Math.round(
+      ((selectedProduct.amount/100)*selectedProduct.sugar)*100
+    )/100;
+
+    meal.fats += Math.round(
+      ((selectedProduct.amount/100)*selectedProduct.fats)*100
+    )/100;
+
+    meal.proteins += Math.round(
+      ((selectedProduct.amount/100)*selectedProduct.protein)*100
+    )/100;
+
+    // Remove selected product
+    selectedProduct = null;
+
+    console.log(meal);
+
+    this.setState({meal, selectedProduct});
+  }
+
+  discardSelectedProduct() {
+    let {selectedProduct} = this.state;
+    selectedProduct = null;
+    this.setState({selectedProduct});
+  }
+
+  getMealCard() {
+    const {meal} = this.state;
+
+    if (meal.products && meal.products.length) {
+      let productsInfo = meal.products.map(product => {
+        return <Paragraph key={'meal-product-data'+product.rowid}>
+          {product.amount} grams of {product.name}/{product.per_amount}kcal
+        </Paragraph>
+      })
+      return <Card mode="outlined" style={{marginTop: 5}}>
+        <Card.Title title={`Current meal: ${meal.calories}kcal.`}/>
+        <Card.Content>
+          {productsInfo}
+        </Card.Content>
+      </Card>
+    }
+
+    return null;
   }
 
   getSelectedItemForm() {
@@ -101,10 +185,10 @@ class AddMeal extends React.Component {
     if (!selectedProduct) return false;
 
     return <Card mode="outlined" style={{marginTop: 5}}>
-      <Card.Title title={selectedProduct.name}></Card.Title>
+      <Card.Title title={`${selectedProduct.name}: 100g/${selectedProduct.calories} kcal`}></Card.Title>
       <Card.Content>
-        <Paragraph>Calories per 100 grams: {selectedProduct.calories}</Paragraph>
-          <TextInput style={{ backgroundColor: null}}
+          {this.caloriesPerAmountParagraph(selectedProduct)}
+          <TextInput style={{ backgroundColor: null, paddingHorizontal: 0}}
             label={`Amount of ${selectedProduct.name} in grams`}
             value={selectedProduct.amount}
             keyboardType="numeric"
@@ -112,8 +196,8 @@ class AddMeal extends React.Component {
           />
       </Card.Content>
       <Card.Actions>
-        <Button>Discard</Button>
-        <Button>Ok</Button>
+        <Button onPress={() => {this.discardSelectedProduct()}}>Discard</Button>
+        <Button onPress={() => {this.addToMeal(selectedProduct)}}>Add to meal</Button>
       </Card.Actions>
     </Card>
   }
@@ -123,6 +207,7 @@ class AddMeal extends React.Component {
 
     let suggestions = this.getSuggestionsList(productsResult)
     let selectedProductForm = this.getSelectedItemForm();
+    let mealData = this.getMealCard();
     return (
       <SafeAreaView style={{minHeight: 450}}>
       <ScrollView style={styles.viewStyle2}>
@@ -135,7 +220,7 @@ class AddMeal extends React.Component {
         {suggestions}
 
         {selectedProductForm}
-
+        {mealData}
       </ScrollView>
       </SafeAreaView>
     )
